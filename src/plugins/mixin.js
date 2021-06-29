cef990eb6e88898ac5a1b6cffe8dbacb9d3fa710
jaydonhu <jaydonhu@tencent.com>
2021-06-29 20:43:25 +0800

    feat: 增加mixin

---
+++ src/plugins/mixin.js
@@ -0,0 +1,11 @@
+/*
+ * @Author : jaydon
+ * @Date   : 2021-06-28 23:38
+ * @WeChat : i-vshow
+ * @Email  : vshow@huxiaodo.com
+ * @Blog   : http://g.huxiaodo.com
+ */
+
+import Vue from 'vue';
+
+Vue.mixin({});
