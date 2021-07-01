a6760cdc8da6510ba652efde2810faffd47a7b80
jaydonhu <jaydonhu@tencent.com>
2021-07-01 13:40:40 +0800

    feat: mixin中增加媒体查询

--- vue.config.js
+++ vue.config.js
@@ -39,7 +39,7 @@ module.exports = {
         ]
       },
       sass: {
-        prependData: `@import "@/styles/variables.scss";`
+        prependData: `@import "@/styles/variables.scss", "@/styles/mixin.scss";`
       }
     }
   },
